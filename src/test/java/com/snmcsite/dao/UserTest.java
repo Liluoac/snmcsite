4e53ea5fcb9c435ca4bd07c8b9956b5dc6f83d58
Taotao <tt416926063@qq.com>
2019-07-17 22:59:12 +0800

    UserDao测试

---
+++ src/test/java/com/snmcsite/dao/UserTest.java
@@ -0,0 +1,17 @@
+package com.snmcsite.dao;
+
+import com.snmcsite.BaseTest;
+import com.snmcsite.entity.User;
+import com.snmcsite.service.UserService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserTest extends BaseTest {
+    @Autowired
+    private UserService userService;
+    @Test
+    public void getUser(){
+        User user= userService.getUser(1);
+        System.out.println(user);
+    }
+}
