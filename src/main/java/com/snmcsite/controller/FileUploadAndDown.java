ded9b81ce27a3f2d90e898640f0c94a0a4e85902
Taotao <tt416926063@qq.com>
2019-07-20 09:36:39 +0800

    小文件上传

---
+++ src/main/java/com/snmcsite/controller/FileUploadAndDown.java
@@ -0,0 +1,22 @@
+package com.snmcsite.controller;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("file")
+public class FileUploadAndDown {
+    @RequestMapping("toUploadFile")
+    public String toUploadFile(){
+        return "fileUpload";
+    }
+
+    @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
+    public String uploadFile(@Param("uploadFile") MultipartFile uploadFile){    //参数名字必须和jsp文件中的name名字一致
+        System.out.println(uploadFile);
+        return "uploadFile";
+    }
+}
