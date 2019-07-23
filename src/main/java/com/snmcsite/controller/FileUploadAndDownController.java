e85d96abba6ef0c7aef703aaf67047512d965523
Taotao <tt416926063@qq.com>
2019-07-23 13:11:36 +0800

    上传文件到/WEB-INF/download文件夹下

--- src/main/java/com/snmcsite/controller/FileUploadAndDownController.java
+++ src/main/java/com/snmcsite/controller/FileUploadAndDownController.java
@@ -2,27 +2,31 @@ package com.snmcsite.controller;
 
 import com.snmcsite.util.UploadFile;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 @RequestMapping("file")
-public class FileUploadAndDown {
+public class FileUploadAndDownController {
+    @Autowired
+    private HttpServletRequest request;
+
     @RequestMapping("toUploadFile")
-    public String toUploadFile(){
+    public String toUploadFile() {
         return "fileUpload";
     }
 
     @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
-    public String uploadFile(@Param("uploadFile") MultipartFile uploadFile,
-                             @Param("uploadFileName") String uploadFileName) throws IOException {    //参数名字必须和jsp文件中的name名字一致
-        System.out.println(uploadFile);
-        System.out.println(uploadFileName);
-        UploadFile.uploadFile(uploadFileName);
+    public String uploadFile(@Param("uploadFile") MultipartFile uploadFile) throws IOException {    //参数名字必须和jsp文件中的name名字一致
+        String path=request.getSession().getServletContext().getRealPath("/");
+
+        UploadFile.uploadFile(uploadFile, path);
         return "uploadFile";
     }
 }
--- src/main/java/com/snmcsite/util/UploadFile.java
+++ src/main/java/com/snmcsite/util/UploadFile.java
@@ -1,30 +1,28 @@
 package com.snmcsite.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.*;
 
 public class UploadFile {
-    private static FileInputStream fileInputStream;
-    private static BufferedInputStream bufferedInputStream;
     private static FileOutputStream fileOutputStream;
     private static BufferedOutputStream out;
 
-    public static void uploadFile(String filePathName) throws IOException {
+    public static void uploadFile(MultipartFile uploadFile,String path) throws IOException {
         try {
-            int index = filePathName.lastIndexOf("\\");
-            String fileName = filePathName.substring(index);
-            fileInputStream = new FileInputStream("F:\\uploadFile\\ChromeSetup.exe");
-            int size = fileInputStream.available();
-            bufferedInputStream = new BufferedInputStream(fileInputStream);
-            fileOutputStream = new FileOutputStream("F:\\uploadFile1\\ChromeSetup2.exe");
+            File file=new File(path+"WEB-INF\\download");
+            if (!file.exists()){
+                file.mkdir();
+            }
+            String fileName=uploadFile.getOriginalFilename();
+            byte[] bytes = uploadFile.getBytes();
+            String path1=file.getPath();
+            fileOutputStream = new FileOutputStream(file.getPath()+"\\"+fileName);
             out = new BufferedOutputStream(fileOutputStream);
-            byte[] bytes = new byte[size];
-            bufferedInputStream.read(bytes, 0, size);
             out.write(bytes);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            fileInputStream.close();
-            bufferedInputStream.close();
             fileOutputStream.close();
             out.close();
         }
